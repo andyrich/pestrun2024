b2221f2fa3709e5e8287af94cd3dd0cf65615025
arich <arich@scwa.ca.gov>
2024-07-18 11:06:58 -0700

    updated ip

--- PEST_PP/worker.sh
+++ PEST_PP/worker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-source /home/ec2-user/miniconda3/etc/profile.d
+su - ec2-user
 conda activate gis_py_10
 pwd
 
 unzip -q data.zip
 cd pestaws
-pestpp-ies pest_ies.pst /h 172.31.46.246:9701
+pestpp-ies pest_ies.pst /h 172.31.36.83:9701
 
