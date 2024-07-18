7c43c833f6c6a98e51cde3c2c69e626aa765f892
arich <arich@scwa.ca.gov>
2024-07-18 10:26:25 -0700

    updates to run 5

--- PEST_PP/worker.sh
+++ PEST_PP/worker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-source /home/ubuntu/anaconda3/etc/profile.d/conda.sh
+source /home/ec2-user/miniconda3/etc/profile.d
 conda activate gis_py_10
 pwd
 
 unzip -q data.zip
 cd pestaws
-pestpp-ies pest_ies.pst /h xxx.xxx.xxx.xxx:9701
+pestpp-ies pest_ies.pst /h 172.31.46.246:9701
 
